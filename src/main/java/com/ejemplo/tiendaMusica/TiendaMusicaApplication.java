3c1767e36771b9a41e2fca9ae5eec1b625a9e1bb
El profe <al.sepulvedam@profesor.duoc.cl>
2025-05-13 12:14:17 -0400

    first commit

---
+++ src/main/java/com/ejemplo/tiendaMusica/TiendaMusicaApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.tiendaMusica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMusicaApplication.class, args);
+	}
+
+}
